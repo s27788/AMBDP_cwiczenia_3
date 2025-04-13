f44f51c51bd322948aaa3717e956f20cd7572df2
s27788 <s27788@pjwstk.edu.pl>
2025-04-13 14:51:08 +0200

    feat: uzupełniono test ShouldReturnAllSalesmen

--- Tutorial4/obj/Debug/net9.0/Tutorial4.AssemblyInfo.cs
+++ Tutorial4/obj/Debug/net9.0/Tutorial4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f743b701e86d10a6572346c2df3e6cad53f725")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0562bd7623841e6b9170a48ea56fd0006bc2202c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
