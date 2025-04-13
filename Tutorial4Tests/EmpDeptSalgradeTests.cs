89f315dfebc3ff29fb7d593be2e9bacdcc0fd0f4
s27788 <s27788@pjwstk.edu.pl>
2025-04-13 15:00:52 +0200

    uzupełniono test ShouldSelectNamesAndSalaries

--- Tutorial4Tests/EmpDeptSalgradeTests.cs
+++ Tutorial4Tests/EmpDeptSalgradeTests.cs
@@ -59,13 +59,15 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        //var result = null; 
-        
-        // Assert.All(result, r =>
-        // {
-        //     Assert.False(string.IsNullOrWhiteSpace(r.EName));
-        //     Assert.True(r.Sal > 0);
-        // });
+        var result = emps
+            .Select(e => new { e.EName, e.Sal })
+            .ToList();
+
+        Assert.All(result, r =>
+        {
+            Assert.False(string.IsNullOrWhiteSpace(r.EName));
+            Assert.True(r.Sal > 0);
+        });
     }
 
     // 5. JOIN Emp to Dept
