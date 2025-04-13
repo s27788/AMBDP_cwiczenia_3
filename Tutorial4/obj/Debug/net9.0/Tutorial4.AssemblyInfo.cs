64090090d2a78fd7c32743b7c9920806d3ea6387
s27788 <s27788@pjwstk.edu.pl>
2025-04-13 14:57:56 +0200

    uzupełniono test ShouldReturnEmployeesFromChicago

--- Tutorial4/obj/Debug/net9.0/Tutorial4.AssemblyInfo.cs
+++ Tutorial4/obj/Debug/net9.0/Tutorial4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44f51c51bd322948aaa3717e956f20cd7572df2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2247698937f6e1edca4974ae599d5c4e88acb395")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tutorial4Tests/EmpDeptSalgradeTests.cs
+++ Tutorial4Tests/EmpDeptSalgradeTests.cs
@@ -41,7 +41,13 @@ public class EmpDeptSalgradeTests
         var emps = Database.GetEmps();
         var depts = Database.GetDepts();
 
-        List<Emp> result = null; 
+        var chicagoDeptNos = depts
+            .Where(d => d.Loc == "CHICAGO")
+            .Select(d => d.DeptNo);
+
+        List<Emp> result = emps
+            .Where(e => chicagoDeptNos.Contains(e.DeptNo))
+            .ToList();
 
         Assert.All(result, e => Assert.Equal(30, e.DeptNo));
     }
