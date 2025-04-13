db37b552b3b9c397ea15788fc151a0db744e1897
s27788 <s27788@pjwstk.edu.pl>
2025-04-13 15:07:29 +0200

    uzup ShouldCalculateAverageSalaryPerDept

--- Tutorial4Tests/obj/Debug/net9.0/Tutorial4Tests.AssemblyInfo.cs
+++ Tutorial4Tests/obj/Debug/net9.0/Tutorial4Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad82418fece9617e9185ffb38952e9abe9cd5edc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61756168df582cc29450b012510674038f150acc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
